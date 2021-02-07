8b43e50352c835c73f05810d4a99fee56413442f
IronJon <lang.jeffrey@gmail.com>
2021-02-07 14:21:54 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   })
   .then(() =>
     console.log(`Successfully connected to database: ${databaseName}`)
