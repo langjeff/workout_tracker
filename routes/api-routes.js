77dbb8e7ab80bbf8fe085cc73f6e2f589a34aa25
IronJon <lang.jeffrey@gmail.com>
2021-02-07 13:32:16 -0700

    update api-routes.js for get, put, and post routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,16 +3,65 @@ const db = require("../models");
 
 //* add routes for ../public api.js
 
-//* getLastWorkout() 
-
+//* getLastWorkout()
+router.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((data) => {
+      res.json(data);
+      console.log(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 //* addExercise()
-
+router.put("/workouts/:id", ({ params, body }, res) => {
+  console.log(body);
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: {
+        exercises: body,
+      },
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 //* createWorkout();
+router.post("/workouts", ({ body }, res) => {
+  console.log(body);
+  db.Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 
+  res.send("Got a POST request");
+});
 
 //* getWorkoutsInRange
-
+router.get("/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
